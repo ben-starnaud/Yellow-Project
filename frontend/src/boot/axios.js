96646fe317e9590f58db1e434fa6e419df4423fc
St Arnaud, BJ, Mr [25116835@sun.ac.za] <25116835@sun.ac.za>
2026-03-12 13:54:07 +0200

    BUG FIX : Axios inconsistancy

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,7 +1,7 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios' 
 
-const api = axios.create({ baseURL: 'http://192.168.0.110:8000' })
+const api = axios.create({ baseURL: process.env.API_URL  || 'http://localhost:8000' })
 
 export default boot(({ app }) => {
   app.config.globalProperties.$api = api
